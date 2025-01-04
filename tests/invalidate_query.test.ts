e0af8f022337671550f025c1ae2beeabca7f871c
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2025-01-04 14:07:10 +0900

    add invalidate query test

---
+++ tests/invalidate_query.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { JSONPathJS } from "../src";
+
+test("Query that does not start with $ should throw an error", () => {
+  expect(() => new JSONPathJS("*")).toThrowError();
+});
+
+test("Query with unclosed brackets should throw an error", () => {
+  expect(() => new JSONPathJS("$.store.book[0")).toThrowError();
+});
